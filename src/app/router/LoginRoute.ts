0345a2112a7d75316e07d5d44d7e172bb796c337
ljutreras <ljutreras96@gmail.com>
2023-10-19 18:05:37 -0300

    feat: add crud to profile and remove crud to login

--- src/app/router/LoginRoute.ts
+++ src/app/router/LoginRoute.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { SignUpController } from "@app/controller/login/post/SignUpController";
-import { SignInController } from "@app/controller/login/post/SignInController";
-import { DeleteUserController } from "@app/controller/login/delete/DeleteUserController";
+import { SignUpController } from "@app/controller/profile/post/SignUpController";
+import { SignInController } from "@app/controller/profile/post/SignInController";
+import { DeleteProfileController } from "@app/controller/profile/delete/DeleteProfileController";
+import { UpdateProfileController } from "@app/controller/profile/put/UpdateProfileController";
+import { GetAllProfileController } from "@app/controller/profile/get/GetAllProfileController";
 
-export const login = express.Router()
+export const profile = express.Router()
 
-login.post('/sign-up', bodyParser.json(), SignUpController)
-login.post('/sign-in', bodyParser.json(), SignInController)
-login.delete('/user/:id', bodyParser.json(), DeleteUserController)
+profile.post('/sign-up', bodyParser.json(), SignUpController)
+profile.post('/sign-in', bodyParser.json(), SignInController)
+profile.delete('/user/:id', bodyParser.json(), DeleteProfileController)
+profile.put('/user/:email', bodyParser.json(), UpdateProfileController)
+profile.get('/user', bodyParser.json(), GetAllProfileController)
