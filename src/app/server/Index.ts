4bc287a0eb276253f48ef2346664f228456a2993
ljutreras <ljutreras96@gmail.com>
2023-10-25 15:30:15 -0300

    add .

--- src/app/server/Index.ts
+++ src/app/server/Index.ts
@@ -7,7 +7,7 @@ import { users } from '../router/UsersRoute';
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'fe-readconnect.vercel.app',
     methods: ['*'],
     allowedHeaders: ['Content-Type'],
 }
